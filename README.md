d1f25455f35344c8af9041fae1c42f918c17e7eb
afekhide <afekhide009@gmail.com>
2024-10-31 09:46:54 +0100

    Initial release

---
+++ README.md
@@ -0,0 +1,9 @@
+# OSTEP/Unix Utilities: cat
+
+
+This project is part of the projects presented in the book `Operating Systems, Three Easy Pieces`. It is a submission for the implementation of the Unix *cat* command. The complete list of exercise projects can found [here](https://github.com/remzi-arpacidusseau/ostep-projects/tree/master) 
+
+**BUILD**
+The code was written as a VS2022 project, although a simple `gcc` command should build the file.
+Using MsBuild: `
+msbuild Pointers.vcxproj /p:configuration=release`
---
+++ wcat.cpp
@@ -0,0 +1,70 @@
+/* """
+* 
+* Submission for the OSTEP Projects
+* Category: Initial Projects/Unix Utilities
+* Project: wcat
+* Author: Dlite/Afekhide
+* 
+* Usage: wcat [file1, file2, file3...]
+* Link to Projects page: aHR0cHM6Ly9naXRodWIuY29tL3JlbXppLWFycGFjaWR1c3NlYXUvb3N0ZXAtcHJvamVjdHM=
+
+""" */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+enum STATUS {
+	FAILED_TO_OPEN_FILE = -1,
+	NO_ARGUMENTS_PASSED = -2,
+	OPERATIONS_SUCCEEDED = 0,
+	ERROR_READING_FILE = -3
+};
+
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+		std::string help_message = R"(Usage: wcat file1, [file2, file3, ...])";
+		std::cout << help_message;
+		return STATUS::NO_ARGUMENTS_PASSED;
+	}
+
+	/*Assuming we passed files to th command, 
+	we can then loop through the arguments array, printing each file content
+	*/
+
+	for (int i = 1; i < argc; i++) {
+		std::cout << "File: " << argv[i] << std::endl;
+		std::string line;
+		std::ifstream file;
+		file.open(argv[i], std::ios_base::in);
+		if (!file.is_open()) {
+			std::cout << "Error opening file: " << argv[i];
+			return STATUS::FAILED_TO_OPEN_FILE;
+		}
+
+		while (file.good()) {
+			std::getline(file, line);
+
+			//Assuming an error prevented the file from being read!
+			if (file.bad()) {
+				std::cout << "Error reading file: " << argv[i] << std::endl;
+				return STATUS::ERROR_READING_FILE;
+			}
+			std::cout << line << "\n";
+			if (file.eof()){
+				std::cout << "\n\n\n";
+				file.close();
+				break;
+			}
+			
+		}
+
+		
+	}
+	std::cout << "\nFinished...";
+	return STATUS::OPERATIONS_SUCCEEDED;
+	
+
+	
+	return 0;
+}
